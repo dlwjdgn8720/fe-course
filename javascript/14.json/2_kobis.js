ef1feafbb2de18fb9b74a8740fa4629321ca59df
hoo <dlwjdgn8720@gmail.com>
2026-04-02 16:37:24 +0900

    2026-04-02 commit

--- javascript/14.json/2_kobis.js
+++ javascript/14.json/2_kobis.js
@@ -16,11 +16,20 @@ const getMovieInfo = async (movieCd) => {
     let response = await fetch(data_url);
     return response.json();
 }
+const handleMovieInfo = async (movieCd, rank, poster, posterObj) => {
+    let info = await getMovieInfo(movieCd);
+    console.log(info);
+    let movieNm = info.movieInfoResult.movieInfo.movieNm;
+    let director = info.movieInfoResult.movieInfo.directors[0].peopleNm;
+    let actors = info.movieInfoResult.movieInfo.actors[0].peopleNm
+    openModal({ movieNm, director, actors, rank, poster, posterObj });
+}
 /** open Modal */
 const openModal = (infoObj) => {
     let modal = document.querySelector('#modal');
     let modalBody = document.querySelector('#modal-body');
     let modalClose = document.querySelector('#modal-close');
+    let posters = infoObj.posterObj.split(',')
 
     modalClose.addEventListener('click', () => {
         modal.style.display = 'none';
@@ -30,6 +39,12 @@ const openModal = (infoObj) => {
     let output = `
         <h3>[${infoObj.rank}]${infoObj.movieNm}</h3>
         <ul>
+            <li> ${posters.map(poster => `
+                        <img src="${poster}" style = "width:100px">`
+    ).join("")
+        }
+                
+            </li>
             <li><label>감독 : </label>${infoObj.director}</li>
             <li><label>배우 : </label>${infoObj.actors}</li>
         </ul>
@@ -39,13 +54,22 @@ const openModal = (infoObj) => {
     modalBody.innerHTML = output;
 }
 
-const handleMovieInfo = async (movieCd, rank) => {
-    let info = await getMovieInfo(movieCd);
-    console.log(info);
-    let movieNm = info.movieInfoResult.movieInfo.movieNm;
-    let director = info.movieInfoResult.movieInfo.directors[0].peopleNm;
-    let actors = info.movieInfoResult.movieInfo.actors[0].peopleNm
-    openModal({movieNm, director, actors, rank});
+
+//poster 가져오기 : KMDB API
+const searchMoviePoster = async (movieNm, openDt) => {
+    const key = '59H5F0U0OFQB3R2261VM';
+    let kmdb_url = `http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api`;
+    kmdb_url += `/search_json2.jsp?collection=kmdb_new2&detail=Y&releaseDts=${openDt}&title=${movieNm}&ServiceKey=${key}`;
+    console.log(kmdb_url);
+
+    let response = await fetch(kmdb_url);
+    let kmdb = await response.json();
+
+    if (kmdb.Data && kmdb.Data[0] && kmdb.Data[0].Result) {
+        return await kmdb.Data[0].Result[0].posters.split("|");
+    } else {
+        return [];
+    }
 }
 
 //handleBoxOffice 함수 정의
@@ -64,13 +88,35 @@ const handleBoxOffice = async () => {
         let kobis = await getJson(type, targetDt);
         let kobisBoxOffice = kobis.boxOfficeResult;
         let kobisBoxOfficeList = null;
+        let posterList = [];
+        let posterObjects = [];
+
         if (type === 'Daily') {
             kobisBoxOfficeList = kobis.boxOfficeResult.dailyBoxOfficeList;
         } else {
             kobisBoxOfficeList = kobis.boxOfficeResult.weeklyBoxOfficeList;
         }
 
-        console.log(kobis);
+        for (const movie of kobisBoxOfficeList) {
+            //영화제목(movieNm), 개봉일(openDt)
+            let movieNm = movie.movieNm;
+            let openDt = movie.openDt.split("-").reduce((acc, cur) => acc + cur);
+            // console.log(movieNm, openDt);
+            let posters = await searchMoviePoster(movieNm, openDt);
+            // console.log(posters);
+            posterObjects.push(posters);
+            //posterList.push(posters);
+            if (posters.length !== 0) {
+                posterList.push(posters[0]);
+            }
+            else {
+                posterList.push('');
+            }
+        }
+
+        //console.log(kobis);
+        console.log(posterList);
+
 
         let output = `
             <h1>${kobisBoxOffice.boxofficeType}</h1>
@@ -85,10 +131,12 @@ const handleBoxOffice = async () => {
                 <th>누적 매출액</th>
             </tr>
 
-        ${kobisBoxOfficeList.map((movie) =>
+        ${kobisBoxOfficeList.map((movie, idx) =>
             `<tr>
                 <td>${movie.rank}</td>
-                <td><a href="#" onclick="handleMovieInfo(${movie.movieCd},${movie.rank})">${movie.movieNm}</td>
+                <td>
+                    <img src="${posterList[idx]}" style="width:100px">
+                    <a href="#" onclick="handleMovieInfo(${movie.movieCd},${movie.rank},'${posterList[idx]}','${posterObjects[idx]}')">${movie.movieNm}</td>
                 <td>${movie.openDt}</td>
                 <td>${parseInt(movie.audiCnt).toLocaleString()}명</td>
                 <td>${parseInt(movie.audiAcc).toLocaleString()}원</td>
