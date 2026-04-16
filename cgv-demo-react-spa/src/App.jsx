a045416c1d22e862c8afe864d9dfaf27f94b0684
hoo <dlwjdgn8720@gmail.com>
2026-04-16 18:26:11 +0900

    2026-04-16 commit

---
+++ cgv-demo-react-spa/src/App.jsx
@@ -0,0 +1,19 @@
+import './cgv.css';
+import './commons.css';
+// import { Outlet } from 'react-router-dom';
+import Header from "./components/Header.jsx"
+import Index from "./pages/Index.jsx"
+import Footer from "./components/Footer.jsx"
+
+export default function App() {
+  
+  return (
+    <>
+      {/* <Header /> */}
+      {/* <Index /> */}
+      {/* <Outlet /> */}
+      {/* <Footer/> */}
+    </>
+  )
+}
+
---
+++ cgv-demo-react-spa/src/main.jsx
@@ -0,0 +1,23 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from './App.jsx'
+import Index from './pages/Index.jsx'
+
+//router 객체 생성
+// const router = createBrowserRouter([
+//   {
+//       path: "/",
+//       element: <App/>, //Layout 구성 컴포넌트 정의 : App.jsx는 미리 생성됨
+//       children: [
+//         {index: true, element: <Index />}
+//       ],
+//     },
+// ]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App/>
+    {/* <RouterProvider router={router} /> */}
+  </StrictMode>,
+)
